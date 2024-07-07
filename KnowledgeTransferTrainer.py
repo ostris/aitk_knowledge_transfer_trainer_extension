7c3f21f97abf39db453dc9b8211c729fd2b461fb
Jaret Burkett <jaretburkett@gmail.com>
2024-07-07 07:29:07 -0600

    Bug fixes

--- KnowledgeTransferTrainer.py
+++ KnowledgeTransferTrainer.py
@@ -1,5 +1,5 @@
 from collections import OrderedDict
-from typing import Optional
+from typing import Optional, Union
 
 from torch.utils.data import DataLoader
 
@@ -8,6 +8,7 @@ from toolkit.clip_vision_adapter import ClipVisionAdapter
 from toolkit.config_modules import ModelConfig
 from toolkit.data_transfer_object.data_loader import DataLoaderBatchDTO
 from toolkit.prompt_utils import concat_prompt_embeds, split_prompt_embeds, PromptEmbeds
+from toolkit.sampler import get_sampler
 from toolkit.stable_diffusion_model import StableDiffusion, BlankNetwork, UNET_IN_CHANNELS
 from toolkit.train_tools import get_torch_dtype, apply_snr_weight
 import gc
@@ -28,12 +29,23 @@ class KnowledgeTransferTrainer(SDTrainer):
         self.device2_dtype = self.get_conf('device2_dtype', self.train_config.dtype)
         self.device2_torch = torch.device(self.device2)
         self.do_prior_prediction = True
+        self.step_prediction = self.get_conf('step_prediction', False)
         self.caption_list = []
+        self.alternate = self.get_conf('alternate', False)
+
+        sampler = get_sampler(
+            self.train_config.noise_scheduler,
+            {
+                "prediction_type": "v_prediction" if self.model_config.is_v_pred else "epsilon",
+            },
+            'sd' if not self.model_config.is_pixart else 'pixart'
+        )
 
         self.sd_source = StableDiffusion(
             device=self.device2,
             model_config=self.model_source_config,
             dtype=self.device2_dtype,
+            noise_scheduler=sampler
         )
 
     def before_model_load(self):
@@ -80,6 +92,8 @@ class KnowledgeTransferTrainer(SDTrainer):
         is_reg = any(batch.get_is_reg_list())
         if is_reg:
             return None
+        if self.alternate and self.step % 2 == 0:
+            return None
         # todo for embeddings, we need to run without trigger words
         was_unet_training = self.sd.unet.training
         was_network_active = False
@@ -102,14 +116,27 @@ class KnowledgeTransferTrainer(SDTrainer):
                 self.device_torch,
                 dtype=dtype).detach()
 
+            unconditional_to_use = None
+            if unconditional_embeds is not None:
+                unconditional_to_use = self.sd_source.encode_prompt(
+                    self.batch_negative_prompt,
+                    long_prompts=self.do_long_prompts).to(
+                    self.device_torch,
+                    dtype=dtype
+                ).detach()
+
             prior_pred = self.sd_source.predict_noise(
                 latents=noisy_latents.to(self.device_torch, dtype=dtype).detach(),
                 conditional_embeddings=embeds.to(self.device_torch, dtype=dtype).detach(),
-                unconditional_embeddings=unconditional_embeds,
+                unconditional_embeddings=unconditional_to_use,
                 timestep=timesteps,
                 guidance_scale=self.train_config.cfg_scale,
                 **pred_kwargs  # adapter residuals in here
             )
+
+            if self.step_prediction:
+                prior_pred = self.sd_source.step_scheduler(prior_pred, noisy_latents, timesteps)
+
             if was_unet_training:
                 self.sd.unet.train()
             prior_pred = prior_pred.detach()
@@ -125,3 +152,31 @@ class KnowledgeTransferTrainer(SDTrainer):
                 self.network.is_active = was_network_active
         return prior_pred
 
+    def calculate_loss(
+            self,
+            noise_pred: torch.Tensor,
+            noise: torch.Tensor,
+            noisy_latents: torch.Tensor,
+            timesteps: torch.Tensor,
+            batch: 'DataLoaderBatchDTO',
+            mask_multiplier: Union[torch.Tensor, float] = 1.0,
+            prior_pred: Union[torch.Tensor, None] = None,
+            **kwargs
+    ):
+        if self.step_prediction:
+            noise_pred = self.sd.step_scheduler(noise_pred, noisy_latents, timesteps)
+
+        # we scale this prediction to match the parent model
+        if self.model_source_config.is_v_pred and not self.model_config.is_v_pred:
+            noise_pred = self.sd_source.noise_scheduler.get_velocity(batch.latents, noise_pred, timesteps)
+
+        return super().calculate_loss(
+            noise_pred=noise_pred,
+            noise=noise,
+            noisy_latents=noisy_latents,
+            timesteps=timesteps,
+            batch=batch,
+            mask_multiplier=mask_multiplier,
+            prior_pred=prior_pred,
+            **kwargs
+        )
